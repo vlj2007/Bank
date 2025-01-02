bcbdda4cda161cf9c1f72d258d5d367603dc7d04
vladimir <vlj2007@yandex.ru>
2025-01-02 21:39:27 +0700

    I added file WithdrawTask.java

---
+++ src/WithdrawTask.java
@@ -0,0 +1,15 @@
+// Поток для снятия денег
+public class WithdrawTask implements Runnable{
+    private BankAccount account;
+    private int amount;
+
+    public WithdrawTask(BankAccount account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        account.withdraw(amount);
+    }
+}
